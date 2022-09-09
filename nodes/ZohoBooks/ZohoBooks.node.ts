107ef40a9974f662fafa621aa348688f9e1f191e
Vrishin Patel <vrishin_patel@outlook.com>
2022-09-09 22:41:50 +0530

    add: operation to get a bill

--- nodes/ZohoBooks/ZohoBooks.node.ts
+++ nodes/ZohoBooks/ZohoBooks.node.ts
@@ -21,13 +21,85 @@ export class ZohoBooks implements INodeType {
 			},
 		],
 		requestDefaults: {
-			baseURL: 'https://books.zoho.com/api/v3',
+			baseURL: 'https://books.zoho.in/api/v3',
 			headers: {
 				Accept: 'application/json',
 				'Content-Type': 'application/json',
 			},
 		},
 		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						// eslint-disable-next-line n8n-nodes-base/node-param-resource-with-plural-option
+						name: 'Bills',
+						value: 'bills',
+					},
+					{
+						name: 'Contact Persons',
+						value: 'contactPersons',
+					},
+					{
+						name: 'Contacts',
+						value: 'contacts',
+					},
+					{
+						name: 'Customer Payments',
+						value: 'customerPayments',
+					},
+					{
+						name: 'Estimates',
+						value: 'estimates',
+					},
+					{
+						name: 'Expenses',
+						value: 'expenses',
+					},
+					{
+						name: 'Invoices',
+						value: 'invoices',
+					},
+					{
+						name: 'Items',
+						value: 'items',
+					},
+					{
+						name: 'Sales Orders',
+						value: 'salesOrders',
+					},
+				],
+				default: 'contacts',
+			},
+			{
+				displayName: 'Operations',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: [ 'Bills' ]
+					}
+				},
+				options: [
+					{
+						name: 'Get',
+						value: 'get',
+						action: 'Get a bill',
+						description: 'Get the details of a bill',
+						routing: {
+							request: {
+								method: 'GET',
+								url: '=/bills/{{$parameter.bill_id}}',
+							},
+						},
+					},
+				],
+				default: 'get'
+			}
 		],
 	};
 }
